e9daad226c76e6c46fe36a4f88b878c0a93d0b89
Sophia787 <91783576+Sophia787@users.noreply.github.com>
2021-11-17 16:16:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+def merge(*lists):
+    new_lists = []
+    for list in lists:
+        new_lists.extend(list)
+    return sorted(new_lists)
 
+
+while True:
+    lists = []
+    k = int(input('Введите k = '))
+    for i in range(k):
+        print(i+1, " список (введите значения списка через пробел): ")
+        nums = [int(i) for i in input('nums = ').split()]
+        lists.append(nums)
+
+    print("Вывод: ", list(merge(*lists)))
